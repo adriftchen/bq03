96de182203088ef05169f775687fbe964c0fa26d
linkzepp <adriftchen@gmail.com>
2020-12-30 12:36:29 +0800

    完成新增預告片功能-錯誤修正

--- api/add_poster.php
+++ api/add_poster.php
@@ -9,7 +9,7 @@ if(!empty($_FILES['poster']['tmp_name'])){
   $data['name']=$_POST['name'];
   $data['sh']=1;
   $data['rank']=$Poster->q("select max(rank) from poster")[0][0]+1;
-  $data['ani']=rank(1,3);
+  $data['ani']=rand(1,3);
   $Poster->save($data);
   
 }
--- base.php
+++ base.php
@@ -2,6 +2,8 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
+$Poster=new DB('poster');
+
 class DB{
     protected $dsn="mysql:host=localhost;dbname=db11;charset=utf8";
     protected $table="";
