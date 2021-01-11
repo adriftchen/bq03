3afa0409a42ad8a4b465272a92a27918fb5e0ce3
linkzepp <adriftchen@gmail.com>
2021-01-11 10:03:34 +0800

    完成訂票頁面建置及取得上映中的電影列表

---
+++ front/order.php
@@ -0,0 +1,55 @@
+<h4 class="ct">線上訂票</h4>
+<form>
+  <table style="width:400px;margin:auto">
+    <tr>
+      <td width=15%>電影:</td>
+      <td><select name="movie" id="movie" style="width:98%"></select></td>
+    </tr>
+    <tr>
+      <td>日期:</td>
+      <td><select name="date" id="date" style="width:98%"></select></td>
+    </tr>
+    <tr>
+      <td>場次:</td>
+      <td><select name="session" id="session" style="width:98%"></select></td>
+    </tr>
+  </table>
+  <div class="ct">
+    <input type="button" value="訂票">
+    <input type="reset" value="重置">
+  </div>
+</form>
+
+<script>
+//院線片新增的電影上映日期只有含今天往前算三天內
+//前端js取得url參數作法
+/*
+let qeury={};
+document.location.search.replace("?","").split("&").forEach(function(item,idx){
+  query[item.split("=")[0]]=item.split("=")[1]
+})
+console.log(query)
+
+if(query.id==undefined){
+  getMovies();
+}else{
+  getMovies(query.id);
+}
+*/
+//isset($_GETp['id'])?$_GET['id']:''; 三元運算 同下簡寫
+getMovies(<?=$_GET['id']??'';?>);
+
+
+  function getMovies(id){
+    let movie;
+    if(id!=undefined){
+      movie=id;
+    }else{
+      movie='all';
+    }
+    console.log(movie)
+    $.get("api/get_movies.php",{movie},function(movies){
+      $("#movie").html(movies)
+    })
+  }
+</script>
